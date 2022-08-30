55cfa2bcb70354fefa07a47d5fd68acb6966dbc2
faciqui Doumbia <bizindavyiguillain02@gmail.com>
2022-08-30 17:21:53 +0200

    rendu

--- src/actions.ts
+++ src/actions.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import { GetAllRecipesCommit, GetAllRecipesRequest } from './actions.types';
+import { GetAllRecipesCommit, GetAllRecipesRequest, GetOneRecipe } from './actions.types';
 import { Recipe } from './types';
 
-// ## HERE Q1 ## //
+// ## HERE Q1 ## //    
 
 export const getAllRecipes = (): GetAllRecipesRequest => {
   return { type: 'GET_ALL_RECIPES_REQUEST', commit: { type: 'GET_ALL_RECIPES_COMMIT' } };
@@ -12,5 +12,9 @@ export const getAllRecipesCommit = (payload: Recipe[]): GetAllRecipesCommit => {
   return { type: 'GET_ALL_RECIPES_COMMIT', payload };
 };
 // ## HERE Q7 ## //
+
+export const getOneRecipes = (recipe: Recipe): GetOneRecipe => {
+  return { type: 'GET_One_RECIPES', recipe };
+};
 
 // ## HERE Q11 ## //
